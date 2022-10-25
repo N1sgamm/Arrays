bd39b6158d703e7f06a725c727ab26361f6ac0bd
N1sgamm <N1sgamm@gmail.com>
2022-10-25 10:54:17 -0300

    Initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static int[] ammo;
+        static void Main(string[] args)
+        {
+            ammo = new int[2];
+            ammo[0] = 1;
+            ammo[1] = 2;
+
+            Console.WriteLine(ammo[0]);
+            Console.ReadKey();
+        }
+        static void Fire()
+        {
+
+        }
+        static void Reload()
+        {
+
+        }
+    }
+}
